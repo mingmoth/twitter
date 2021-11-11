545ba46ac480067e56df034e4e8b905a07f201ff
mingmoth <mingmoth@gmail.com>
2021-11-11 17:30:34 +0800

    fix Tweetmodal css & add router-link in Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,22 +4,22 @@
     <div class="nav-option">
       <div class="nav-option-item">
         <div class="nav-option-item-icon" id="homePage"></div>
-        <span class="nav-option-item-label">首頁</span>
+        <router-link to="/main" class="nav-option-item-label">首頁</router-link>
       </div>
       <div class="nav-option-item">
         <div class="nav-option-item-icon" id="userInfo"></div>
-        <span class="nav-option-item-label">個人資料</span>
+        <router-link to="/user/tweets" class="nav-option-item-label">個人資料</router-link>
       </div>
       <div class="nav-option-item">
         <div class="nav-option-item-icon" id="settings"></div>
-        <span class="nav-option-item-label">設定</span>
+        <router-link to="/settings" class="nav-option-item-label">設定</router-link>
       </div>
       <button class="btn-nav" data-bs-toggle="modal"
           data-bs-target="#main-post-tweet"></button>
     </div>
     <div class="nav-logout">
       <div class="nav-logout-icon"></div>
-      <span class="nav-logout-label">登出</span>
+      <router-link to="/login" class="nav-logout-label">登出</router-link>
     </div>
   </div>
 </template>
--- src/components/TweetModal.vue
+++ src/components/TweetModal.vue
@@ -1,34 +1,35 @@
 <template>
-  <div
-    class="modal fade"
-    id="main-post-tweet"
-    aria-hidden="true"
-  >
+  <div class="modal fade" id="main-post-tweet" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <div class="modal-close" data-bs-dismiss="modal"
-            aria-label="Close"></div>
+          <div
+            class="modal-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></div>
         </div>
         <div class="modal-body">
-          <div class="modal-body-icon"></div>
-          <form action="" class="modal-body-tweet">
-            <textarea
-              name="tweet"
-              id="tweet"
-              cols="30"
-              rows="10"
-              placeholder="有什麼新鮮事?"
-            ></textarea>
-            <button
-              class="btn-tweet"
-              id="btn-tweet"
-              data-bs-toggle="modal"
-              data-bs-target="#main-post-tweet"
-            >
-              推文
-            </button>
-          </form>
+          <div class="modal-body-post">
+            <div class="modal-body-post-icon"></div>
+            <form action="" class="modal-body-post-body">
+              <textarea
+                name="tweet"
+                id="tweet"
+                cols="30"
+                rows="10"
+                placeholder="有什麼新鮮事?"
+              ></textarea>
+              <button
+                class="btn-tweet"
+                id="btn-tweet"
+                data-bs-toggle="modal"
+                data-bs-target="#main-post-tweet"
+              >
+                推文
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
