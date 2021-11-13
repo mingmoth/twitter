0c5182e03e0c06335462b034f6ceea6bd680e84b
mingmoth <mingmoth@gmail.com>
2021-11-13 13:48:35 +0800

    modify router & add linkactive css for navPills

--- src/router/index.js
+++ src/router/index.js
@@ -6,81 +6,78 @@ import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'root',
-    redirect: '/login'
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: Login
-  },
-  {
-    path: '/adminlogin',
-    name: 'adminlogin',
-    component: () => import('../views/AdminLogin.vue')
-  },
-  {
-    path: '/register',
-    name: 'register',
-    component: () => import('../views/Register.vue')
-  },
-  {
-    path: '/main',
-    name: 'main',
-    component: Main
-  },
-  {
-    path: '/posts',
-    name: 'posts',
-    component: () => import('../views/Posts.vue')
-  },
-  {
-    path: '/admin/main',
-    name: 'admin-main',
-    component: () => import('../views/AdminMain.vue')
-  },
-  {
-    path: '/admin/users',
-    name: 'admin-users',
-    component: () => import('../views/AdminUsers.vue')
-  },
-  {
-    path: '/user/tweets',
-    name: 'user-tweets',
-    component: () => import('../views/UserTweets.vue')
-  },
-  {
-    path: '/user/replies',
-    name: 'user-replies',
-    component: () => import('../views/UserReplies.vue')
-  },
-  {
-    path: '/user/likes',
-    name: 'user-likes',
-    component: () => import('../views/UserLikes.vue')
-  },
-  {
-    path: '/user/followings',
-    name: 'user-followings',
-    component: () => import('../views/UserFollowings.vue')
-  },
-  {
-    path: '/settings',
-    name: 'settings',
-    component: () => import('../views/Settings.vue')
-  },
-  {
-    path: '*',
-    name: 'not-found',
-    component: NotFound
-  },
-]
-
-const router = new VueRouter({
-  routes
+export default new VueRouter({
+  linkExactActiveClass: 'active',
+  routes : [
+    {
+      path: '/',
+      name: 'root',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/adminlogin',
+      name: 'adminlogin',
+      component: () => import('../views/AdminLogin.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/Register.vue')
+    },
+    {
+      path: '/main',
+      name: 'main',
+      component: Main
+    },
+    {
+      path: '/posts',
+      name: 'posts',
+      component: () => import('../views/Posts.vue')
+    },
+    {
+      path: '/admin/main',
+      name: 'admin-main',
+      component: () => import('../views/AdminMain.vue')
+    },
+    {
+      path: '/admin/users',
+      name: 'admin-users',
+      component: () => import('../views/AdminUsers.vue')
+    },
+    {
+      path: '/user/tweets',
+      name: 'user-tweets',
+      component: () => import('../views/UserTweets.vue')
+    },
+    {
+      path: '/user/replies',
+      name: 'user-replies',
+      component: () => import('../views/UserReplies.vue')
+    },
+    {
+      path: '/user/likes',
+      name: 'user-likes',
+      component: () => import('../views/UserLikes.vue')
+    },
+    {
+      path: '/user/followings',
+      name: 'user-followings',
+      component: () => import('../views/UserFollowings.vue')
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: () => import('../views/Settings.vue')
+    },
+    {
+      path: '*',
+      name: 'not-found',
+      component: NotFound
+    },
+  ]
 })
-
-export default router
